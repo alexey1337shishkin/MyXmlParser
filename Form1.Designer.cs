29ccd8c507d9e639a84e0c4b1d2924d112c5a013
alexey1337shishkin <83724646+alexey1337shishkin@users.noreply.github.com>
2022-03-22 18:19:58 +0300

    1

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace myXmlParser
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.buttonOpenFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(225, 61);
+            this.richTextBox2.TabIndex = 0;
+            this.richTextBox2.Text = "";
+            // 
+            // buttonOpenFile
+            // 
+            this.buttonOpenFile.Location = new System.Drawing.Point(12, 79);
+            this.buttonOpenFile.Name = "buttonOpenFile";
+            this.buttonOpenFile.Size = new System.Drawing.Size(225, 48);
+            this.buttonOpenFile.TabIndex = 1;
+            this.buttonOpenFile.Text = "Открыть файл";
+            this.buttonOpenFile.UseVisualStyleBackColor = true;
+            this.buttonOpenFile.Click += new System.EventHandler(this.buttonOpenFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(250, 136);
+            this.Controls.Add(this.buttonOpenFile);
+            this.Controls.Add(this.richTextBox2);
+            this.Name = "Form1";
+            this.Text = "Парсер XML";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button buttonOpenFile;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace myXmlParser
+{
+    public partial class Form1 : Form
+    {
+        double tempSumm = 0;
+        string[] files = new string[10000];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOpenFile_Click(object sender, EventArgs e)
+        {
+            files = new string[10000];
+            tempSumm = 0;
+
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    files = Directory.GetFiles(fbd.SelectedPath);
+
+                    System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+                }
+            }
+
+            foreach (var item in files)
+            {
+                tempSumm += Checker(item);
+            }
+            tempSumm = Math.Round(tempSumm, 2, MidpointRounding.AwayFromZero);
+            richTextBox2.Text = tempSumm.ToString();
+
+        }
+        public double Checker(string filePath)
+        {
+            XmlDocument A = new XmlDocument();
+            A.Load(filePath);
+            double tempSum = 0;
+            XmlNodeList nodeList = A.GetElementsByTagName("ALLCOST");
+            foreach (XmlElement element in nodeList)
+            {
+                string temp = element.InnerText;
+                tempSum += Convert.ToDouble(temp.Replace('.', ','));
+            }
+            return tempSum;
+        }
+    }
+
+    
+}
